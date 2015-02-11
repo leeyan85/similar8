f05887fb2561175091f2cc52c04fa05d49950426
Lee Yan <leeyan85@gmail.com>
2015-02-11 21:49:22 +0800

    generator and iterator

--- day3/main/iterator_lesson.py
+++ day3/main/iterator_lesson.py
@@ -2,37 +2,37 @@
 #coding: utf-8
 def MyGenerator(*args):
     for i in args:
-        yield i        
+        yield i
 def Lee(name,age):
     return 'I am %s,I am %d old' %(name,age)
-def Marlon(name):
-    return 'I am %s' %(name)
-def Allen(name):
-    return 'I am %s' %(name)
+def Marlon():
+    return 'I am Marlon'
+def Allen():
+    return 'I am Allen'
 
 function_list=  [Lee,Marlon,Allen]  
 a=MyGenerator(*function_list)
 print apply(a.next(),('Lee',29))
-print apply(a.next(),('Marlon',))
-return_value = apply(a.next(),('Allen',))
+print apply(a.next())
+return_value = apply(a.next())
 print return_value
 
 
         
 class MyIterator(object):
     def __init__(self,funcs):
-        self.total_step=len(funcs)
+        self.total_funcs=len(funcs)
         self.func_list=funcs
         self.step=0
     def __iter__(self):
-        return self      
+        pass
     def next(self):        
-        if self.step < self.total_step :
+        if self.step < self.total_funcs:
             self.step+=1
             return self.func_list[self.step-1]
         else:
             raise StopIteration       
 c=MyIterator(function_list)
 print apply(c.next(),('Lee',29))
-print apply(c.next(),('Marlon',))
-print apply(c.next(),('Allen',))
+print apply(c.next())
+print apply(c.next())
