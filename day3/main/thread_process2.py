5899adb9879cc824ce5eb6ca6a1bd228553d81fb
Lee Yan <leeyan85@gmail.com>
2015-02-13 18:37:30 +0800

    thread process learning

--- day3/main/thread_process2.py
+++ day3/main/thread_process2.py
@@ -5,18 +5,18 @@ counter = 0
 mutex = threading.Lock()
    
 class  MyThread(threading.Thread):  
-     def  __init__(self):  
+     def  __init__(self,name):  
         threading.Thread.__init__(self)  
-      
-     def run( self ):  
-         global counter, mutex
+        self.name = "Thread-" + str(name)
+     def run(self):  
+         global counter,mutex
          time.sleep(1);  
          if mutex.acquire():
             counter += 1   
-            print "I am %s, set counter:%s"  % (self.name, counter)  
+            print "I am %s, set counter:%s"  % (self.name,counter)  
             mutex.release()
             
 if  __name__ ==  "__main__":  
-    for i in range(0,100):  
-        my_thread = MyThread()
+    for i in xrange(1,101):  
+        my_thread = MyThread(i)
         my_thread.start()
